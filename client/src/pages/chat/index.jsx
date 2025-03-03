8b2806d050a5d864331d75b2dfd9d0e073ec9fe6
ABHISHEK BHATIA <abhishekbhatia@ABHISHEKs-MacBook-Air.local>
2025-03-03 10:11:03 -0800

    pending work

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppStore } from '../../store'
+import { useNavigate } from 'react-router';
+
 
 const Chat = () => {
+  const {userInfo} = useAppStore();
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if(!userInfo.profileSetup){
+      console.log("please profile steup");
+      navigate("/profile")
+    }
+  },[userInfo,navigate])
   return (
     <div>
       chat
+   
     </div>
   )
 }
